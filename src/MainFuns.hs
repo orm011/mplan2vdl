e77104168c8cc47398daa2250e9062ef72d1e740
Oscar Moll <orm@csai.mit.edu>
2016-06-02 21:39:25 -0400

    remoce 035 config stuff, just use cmdargs

--- src/MainFuns.hs
+++ src/MainFuns.hs
@@ -1,68 +1,45 @@
-{- Main -- main entry point
-Copyright (C) 2013, 2014  Benjamin Barenblat <bbaren@mit.edu>
-
-This file is a part of decafc.
-
-decafc is free software: you can redistribute it and/or modify it under the
-terms of the MIT (X11) License as described in the LICENSE file.
-
-decafc is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the X11 license for more details. -}
 module MainFuns (main, fromFile) where
 
-import Prelude hiding (readFile)
-import qualified Prelude
-
-import Control.Exception (bracket)
-import Control.Monad (forM_, void)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Except (ExceptT(..), runExceptT)
-import Data.Either (partitionEithers)
-import GHC.IO.Handle (hDuplicate)
 import System.Environment (getProgName)
 import qualified System.Exit
 import System.IO (IOMode(..), hClose, hPutStrLn, openFile, stdout, stderr)
 import Text.Printf (printf)
 import Text.Groom (groom)
 
-import Data.List.Utils (replace, startswith)
+import Data.List.Utils (startswith)
 import Data.String.Utils (lstrip)
 
-import qualified CLI
-import Configuration (Configuration, CompilerStage(..))
-import qualified Configuration
 import qualified Voodoo as V
-import qualified Name as Name
+import System.Console.CmdArgs.Implicit
 
 ------------------------ Impure code: Fun with ExceptT ------------------------
 
+data Mplan2Vdl = Mplan2Vdl  {mplanfile :: String
+                           , groupwidths :: Int
+                           } deriving (Show, Data, Typeable)
+
+cmdTemplate = Mplan2Vdl
+  { mplanfile = def &= args &= typ "FILE"
+  , groupwidths = def &= opt "8192" &= typ "INT" &= help "Grain size for foldSum/foldMax/etc"
+  }
+  &= summary "Mplan2Vdl transforms monetDB logical plans to voodoo"
+
 main :: IO ()
 main = do
-  {- Compiler work can be split into three stages: reading input (impure),
-  processing it (pure), and writing output (impure).  Of course, input might be
-  malformed or there might be an error in processing.  Thus, it makes most
-  sense to think of the compiler as having type ExceptT String IO [IO ()] --
-  that is, computation might fail with a String or succeed with a series of IO
-  actions. -}
-  result <- runExceptT $ do
-    -- Part I: Get input
-    configuration <- ExceptT CLI.getConfiguration
-    contents <- readFile $ Configuration.input configuration
-    let lins = lines contents
-    let ignore ln = let stripped = lstrip ln in
-          (startswith "#" stripped) || ( startswith "%" stripped)
-    let justThePlan = filter (not . ignore) lins
-    -- Part II: Process it
-    hoistEither $ V.vdlFromMplan $ concat justThePlan
-  case result of
-    -- Part III: Write output
+  args <- cmdArgs cmdTemplate
+  if mplanfile args == []
+    then (hPutStrLn stderr (printf "Usage: need an input filename (see --help)")
+         >> System.Exit.exitFailure)
+    else return  ()
+  contents <- readFile $ mplanfile args
+  let lins = lines contents
+  let ignore ln = ( startswith "#" stripped) || ( startswith "%" stripped)
+        where stripped = lstrip ln
+  let justThePlan = concat $ filter (not . ignore) lins
+  let res = V.vdlFromMplan justThePlan
+  case res of
     Left errorMessage -> fatal errorMessage
     Right vl -> putStrLn $ vl
-  where hoistEither = ExceptT . return
-
-readFile :: FilePath -> ExceptT String IO String
-readFile name = liftIO $ Prelude.readFile name
 
 fatal :: String -> IO ()
 fatal message = do
@@ -72,6 +49,6 @@ fatal message = do
 
 fromFile :: String -> IO (Either String [(Int, V.Vref)])
 fromFile f =
-  do input <- Prelude.readFile f
+  do input <- readFile f
      let sol = V.fromString input
      return sol
