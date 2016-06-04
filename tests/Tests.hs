cb9bfc2210b79e433cc2cd47a129b7b28b79581a
Oscar Moll <orm@csai.mit.edu>
2016-06-03 21:57:15 -0400

    tests is obsolete

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,30 +1,30 @@
-import Test.Tasty
+--import Test.Tasty
 {- import Test.Tasty.SmallCheck as SC
 import Test.Tasty.QuickCheck as QC -}
-import Test.Tasty.HUnit
-import Data.Either(isRight,partitionEithers, rights)
-import qualified Data.Text as T
-import Text.Groom
-import Debug.Trace
-import Data.List(intercalate)
-import qualified Name as N
+-- import Test.Tasty.HUnit
+-- import Data.Either(isRight)
+-- import qualified Data.Text as T
+-- --import Text.Groom
+-- import Debug.Trace
+-- import Data.List(intercalate)
 
+--Import qualified Parser as P
+--import qualified Mplan as M
+--import qualified Vlite as V
 
-import qualified Parser as P
-import qualified Mplan as M
-import qualified Vlite as V
-
-import Control.DeepSeq(($!!), NFData)
+--import Control.DeepSeq(NFData)
 
 main :: IO ()
-main = do adhoc <- readFile "tests/ad_hoc_tests.txt"
-          tpch <- readFile "tests/tpch_query_plans.txt"
-          detailed  <- readFile "tests/detailed_tests.txt"
-          let adhoc_cases = splitFileIntoTests adhoc
-          let tpch_cases = splitFileIntoTests tpch
-          let detailed_cases = splitFileIntoTests detailed
-          defaultMain $ testGroup "Tests"
-           [ -- testGroup "AdHocParseTests" $ makeTestTree "parse" P.fromString adhoc_cases,
+main = putStrLn "runs nothing"
+
+  -- do adhoc <- readFile "tests/ad_hoc_tests.txt"
+  --         tpch <- readFile "tests/tpch_query_plans.txt"
+  --         detailed  <- readFile "tests/detailed_tests.txt"
+  --         let adhoc_cases = splitFileIntoTests adhoc
+  --         let tpch_cases = splitFileIntoTests tpch
+  --         let detailed_cases = splitFileIntoTests detailed
+  --         defaultMain $ testGroup "Tests"
+--           [ -- testGroup "AdHocParseTests" $ makeTestTree "parse" P.fromString adhoc_cases,
              -- testGroup "TPCHParseTests" $ makeTestTree "parse" P.fromString tpch_cases,
              -- testGroup "DetailedParseTests" $ makeTestTree "parse" P.fromString detailed_cases,
 
@@ -32,33 +32,33 @@ main = do adhoc <- readFile "tests/ad_hoc_tests.txt"
              -- testGroup "TPCHMplanGenTests" $ makeTestTree "mplan" M.fromString tpch_cases,
              -- testGroup "AdHocVliteGenTests" $ makeTestTree "vlite" V.fromString adhoc_cases,
              -- testGroup "TPCHVliteGenTests" $ makeTestTree "vlite" V.fromString tpch_cases,
-             testGroup "end" []
-           ]
+           --   testGroup "end" []
+           -- ]
 
-makeTestName :: String ->  String -> String
-makeTestName name plantext =
-  let zpd = zip [1..] (T.splitOn (T.pack "\n") (T.pack plantext))
-      numline (num, packedline) = (show num) ++ (if num < 10 then " " else "") ++" " ++ (T.unpack packedline)
-      numbered_plan = map numline zpd
-      in "------\n" ++  name ++ "\n" ++ (intercalate "\n" numbered_plan) ++ "\n------\n"
+-- makeTestName :: String ->  String -> String
+-- makeTestName name plantext =
+--   let zpd = zip [1..] (T.splitOn (T.pack "\n") (T.pack plantext))
+--       numline (num, packedline) = (show num) ++ (if num < 10 then " " else "") ++" " ++ (T.unpack packedline)
+--       numbered_plan = map numline zpd
+--       in "------\n" ++  name ++ "\n" ++ (intercalate "\n" numbered_plan) ++ "\n------\n"
 
-splitFileIntoTests :: String -> [(String, String)]
-splitFileIntoTests s =
-  {- the first element after split is an empty string, because we start with a plan, so must do tail -}
-  let rawPairs = case (T.splitOn (T.pack "--TEST--") (T.pack s)) of
-                    [] -> []
-                    _ : rest -> rest
-      toPairs [x,y] = (T.unpack . T.strip $ x, T.unpack . T.strip $ y)
-      in map (toPairs . (T.splitOn (T.pack ";"))) rawPairs
+-- splitFileIntoTests :: String -> [(String, String)]
+-- splitFileIntoTests s =
+--   {- the first element after split is an empty string, because we start with a plan, so must do tail -}
+--   let rawPairs = case (T.splitOn (T.pack "--TEST--") (T.pack s)) of
+--                     [] -> []
+--                     _ : rest -> rest
+--       toPairs [x,y] = (T.unpack . T.strip $ x, T.unpack . T.strip $ y)
+--       in map (toPairs . (T.splitOn (T.pack ";"))) rawPairs
 
-makeTestTree :: (Show a, NFData a) => String -> (String -> Either String a) -> [(String,String)] -> [TestTree]
-makeTestTree compilername compiler  pairs   = map helper pairs
-  where helper (a, b)  = let plainName  = makeTestName a b
-                             forlog = "\n--TEST--\n--SQL:\n" ++ a ++ "\n\n--MonetPlanString:\n" ++ b
-                             prs = compiler (trace forlog b)
-                             detailedName = compilername ++ plainName ++ "\n\n"
-                             msg = "failed " ++ compilername  ++ " test"
-                             tc = testCase detailedName $ (isRight prs) @? msg
-                             in localOption (mkTimeout 1000000) {-100 milliseconds-} tc
+-- makeTestTree :: (Show a, NFData a) => String -> (String -> Either String a) -> [(String,String)] -> [TestTree]
+-- makeTestTree compilername compiler  pairs   = map helper pairs
+--   where helper (a, b)  = let plainName  = makeTestName a b
+--                              forlog = "\n--TEST--\n--SQL:\n" ++ a ++ "\n\n--MonetPlanString:\n" ++ b
+--                              prs = compiler (trace forlog b)
+--                              detailedName = compilername ++ plainName ++ "\n\n"
+--                              msg = "failed " ++ compilername  ++ " test"
+--                              tc = testCase detailedName $ (isRight prs) @? msg
+--                              in localOption (mkTimeout 1000000) {-100 milliseconds-} tc
 
 
