69fdc7bc3e8ccfa6a307396b5d0dddd26dea6ba2
Oscar Moll <orm@csai.mit.edu>
2016-06-11 11:09:22 -0700

    j4?

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #declare -r TOP="$(git rev-parse --show-toplevel)"
 #cd "$TOP"
 set -x
-stack build --test --no-run-tests --ghc-options="-O0" --executable-profiling --library-profiling
+stack build -j 4 --test --no-run-tests --ghc-options="-O0" --executable-profiling --library-profiling
 
 #test makes sure the test executables get built by default (to ensure they still build)
 #noruntest makes sure you build them but not run them (use the test exec ofr that)
