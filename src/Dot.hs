88f7b93ca01f661e1e00ce5e7a40b125a6f4d662
Oscar Moll <orm@csai.mit.edu>
2016-06-12 15:31:02 -0700

    show all attributes  in dot

--- src/Dot.hs
+++ src/Dot.hs
@@ -5,13 +5,14 @@ import Name ()
 import Data.List(foldl')
 import Text.Printf (printf)
 import Data.String.Utils(join,replace)
-import Data.List.NonEmpty 
+import Data.List.NonEmpty hiding (map, last, reverse,zip)
+import qualified Data.List.NonEmpty as N
 import Prelude hiding (map,last,reverse)
 import qualified Prelude as P
 
 --import Debug.Trace
 
-data Dot = Dot (NonEmpty (Int, String, String)) [(Int,Int)]
+data Dot = Dot (NonEmpty (Int, String, [String])) [(Int,Int)]
 
 cat :: NonEmpty a -> NonEmpty a -> NonEmpty a
 cat a b = foldr (<|) b a -- b is the accumulate
@@ -19,21 +20,18 @@ cat a b = foldr (<|) b a -- b is the accumulate
 toDot :: Int -> TRel -> (Int, Dot)
 
 toDot n (TNode { relop, children, arg_lists }) =
-  let firststr = case arg_lists of
-        [] -> ""
-        s : _ -> s
-      merge (m, directChildrenIds, labels1, edges1) child =
+  let merge (m, directChildrenIds, labels1, edges1) child =
         let (m', (Dot labels2 edges2)) = toDot m child
-            (l,_,_) = last labels2 -- the direct child is the last element
+            (l,_,_) = N.last labels2 -- the direct child is the last element
             ans = (m', l:directChildrenIds, cat labels2 labels1 , edges2 ++ edges1)
         in ans
-      initial = (n+1, [], (n,relop,firststr):|[], [])
+      initial = (n+1, [], (n,relop,arg_lists):|[], [])
       (n', childids, acclabels, accedges) = foldl' merge initial children
       chedges = P.map (\ch -> (n,ch)) (P.reverse childids) -- the fold has reversed them
   in (n', Dot acclabels (chedges ++ accedges))
 
 
-toDot n (TLeaf name  attr) = (n+1, Dot ((n,"table " ++ show name,attr) :| []) [])
+toDot n (TLeaf name  attr) = (n+1, Dot ((n,"table " ++ show name,[attr]) :| []) [])
 
 fromTRelToDot :: TRel -> Dot
 fromTRelToDot r = snd $ toDot 0 r
@@ -44,14 +42,16 @@ fromDotToDotString gname (Dot labels edges) =
       epilogue = ["}"]
       printnodelabel (n, labelname, _) =
         printf "%d [ label = \"[%d] %s\" ]" n n labelname
-      printnodemeta (n, _, attrstr) =
-        let attrnode = (show n) ++ "00000" -- hack to avoid node node collision
-        in ( printf "%s [ label=\"%s\" shape = \"box\" color=\"blue\" ]" attrnode (replace "\"" "" attrstr)
-           , printf "%d -> %s [style=\"dotted\" color=\"blue\"]" n attrnode
-           )
+      printnodemeta (n, _, attrs) =
+        do (attr,i) <- zip attrs [0..]
+           let cleanattr = replace " ," ", "  $ replace "\"" "" attr --quotations confuse me and confuse dot
+           let attrnode = show $ (n + 1) * 10000 + i -- +1 bc zero id
+           return $ ( printf "%s [ label=\"%s\" shape = \"box\" color=\"blue\" ]" attrnode cleanattr
+                    , printf "%d -> %s [style=\"dotted\" color=\"blue\"]" n attrnode
+                    )
       printedge (a,b) = printf "%d -> %d" a b
       operlabels = P.map printnodelabel (P.reverse $ toList labels)
-      (metalabels,metaedges) = P.unzip (P.map printnodemeta $ toList labels)
+      (metalabels,metaedges) = P.unzip (P.foldMap printnodemeta $ toList labels)
       operedges = (P.map printedge edges)
       alllines = foldl' (++) [] [prologue, metalabels, operlabels, metaedges, operedges, epilogue]
   in  join "\n" alllines
