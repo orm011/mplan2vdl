c0330da33bfaa0bb3155dbd94fa6579fe1c13714
Oscar Moll <orm@csai.mit.edu>
2016-09-15 14:25:23 -0400

    assertion was too strong for positions smaller than group size

--- src/Vlite.hs
+++ src/Vlite.hs
@@ -998,14 +998,14 @@ make2LevelFold config foldop fgroups fdata =
           let pos = pos_ fgroups
               log_gsize = const_ gsz fgroups
               ones = ones_ fgroups
-          -- example: grainsize = 1. then log_gsize = 0. want 01010101... formula gives (pos >> 0) | 1 = 01010101...
-          -- example: grainsize = 2. then log_gisze = 1. want 00110011... formulate gives (pos >> 1) | 1 = 00110011
-          -- example: grainsize = 4. then log_gisze = 2. want 00001111... formulate gives (pos >> 2) | 1 ...
+              -- example: grainsize = 1. then log_gsize = 0. want 01010101... formula gives (pos >> 0) & 1 = 01010101...
+              -- example: grainsize = 2. then log_gisze = 1. want 00110011... formulate gives (pos >> 1) & 1 = 00110011
+              -- example: grainsize = 4. then log_gisze = 2. want 00001111... formulate gives (pos >> 2) & 1 ...
               level1par = (pos >>. log_gsize) &. ones
               level1groups = composeKeys fgroups level1par
               level1results = complete $ Fold { foldop, fgroups=level1groups, fdata }
               level2results = Fold { foldop, fgroups, fdata=level1results }
-          in assert (getBitWidth level1par == 1) level2results
+          in assert (traceShow (getBitWidth level1par) (getBitWidth level1par <= 1)) level2results
   in complete $ ans
 
 
